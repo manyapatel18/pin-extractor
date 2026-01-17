2a94906ae11c329c57d576e5218673a939b6cdd7
Manya Patel <manyapatel1812@gmail.com>
2026-01-17 09:26:33 +0530

    Add files via upload

---
+++ pinextractor.py
@@ -0,0 +1,16 @@
+poem = '''Stars and the moon
+shine in the sky
+white and bright
+until the end of the night'''
+
+def pin_extractor(poem):
+    secret_code = ''
+    lines = poem.split('\n')
+    
+    for line_index, line in enumerate(lines):
+        words = line.split()
+        secret_code += str(len(words[line_index]))
+    
+    return secret_code
+
+pin_extractor(poem)
